bb751dc0e538f55a06723be9572aa5bcc716a865
Chasky <jporras24@gmail.com>
2014-02-24 22:05:51 -0500

    Calcular chanchita

---
+++ app1/Mi Primera Aplicacion/Mi Primera AplicacionTests/Mi_Primera_AplicacionTests.m
@@ -0,0 +1,34 @@
+//
+//  Mi_Primera_AplicacionTests.m
+//  Mi Primera AplicacionTests
+//
+//  Created by iOS on 2/19/14.
+//  Copyright (c) 2014 JavCBDev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Mi_Primera_AplicacionTests : XCTestCase
+
+@end
+
+@implementation Mi_Primera_AplicacionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ app2/APP2/APP2/TwitterViewController.h
@@ -0,0 +1,14 @@
+//
+//  TwitterViewController.h
+//  APP2
+//
+//  Created by iOS on 2/24/14.
+//  Copyright (c) 2014 JavCBDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UITextView *twdescrip;
+
+@end
