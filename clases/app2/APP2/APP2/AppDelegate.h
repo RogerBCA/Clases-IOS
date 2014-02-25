5224be24bab362ea0ff0f4c5d1fe3e461e8f8c8e
Chasky <jporras24@gmail.com>
2014-02-24 20:08:41 -0500

    Primera Aplicacion

---
+++ clases/app2/APP2/APP2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APP2
+//
+//  Created by iOS on 2/19/14.
+//  Copyright (c) 2014 JavCBDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
